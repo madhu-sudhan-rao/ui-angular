a1ff1580e8c6124debdf197f07c2a0cd9067ccac
unknown <madhuspediredla@gmail.com>
2024-01-20 16:56:00 +0530

    single checkbox strike done

--- done-strike-feature/src/app/app.module.ts
+++ done-strike-feature/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
